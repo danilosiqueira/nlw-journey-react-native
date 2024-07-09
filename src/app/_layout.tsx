a5bdb69cbfbea60f620a98453382eb1557d3b040
Danilo da Silva Siqueira <ssiqueira.danilo@gmail.com>
2024-07-09 10:04:21 -0300

    Criação do projeto até a criação do componente de loading.

---
+++ src/app/_layout.tsx
@@ -0,0 +1,32 @@
+import "@/styles/global.css";
+import { 
+    useFonts, 
+    Inter_500Medium, 
+    Inter_400Regular, 
+    Inter_600SemiBold
+} from "@expo-google-fonts/inter";
+
+
+import { View, StatusBar } from "react-native";
+import { Slot } from "expo-router";
+
+import { Loading } from "@/components/loading";
+
+export default function Layout() {
+    const [fonstLoaded] = useFonts({
+        Inter_500Medium, 
+        Inter_400Regular, 
+        Inter_600SemiBold
+    })
+
+    if (!fonstLoaded) {
+        return <Loading />
+    }
+
+    return (
+        <View className="flex-1 bg-zinc-950">
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+            <Slot/>
+        </View>
+    )
+}
